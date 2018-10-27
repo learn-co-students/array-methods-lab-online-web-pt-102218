d5b75613af39a6b49c7502001441ffde827077ef
Evan Sturman <evan.sturman@gmail.com>
2018-10-27 21:19:59 +0000

    automatically backed up by learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -7,17 +7,17 @@ array.sort(element)
 end
 
 def using_reverse(array)
-
+array.reverse(element)
 end
 
 def using_first(array)
-
+array.first(element)
 end
 
 def using_last(array)
-
+array.last(element)
 end
 
 def using_size(array)
-
+array.size(element)
 end
