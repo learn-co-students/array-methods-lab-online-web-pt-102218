37be74099e0333385e41d8d2813dc4d1c3788989
Evan Sturman <evan.sturman@gmail.com>
2018-10-27 21:27:00 +0000

    automatically backed up by learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,23 @@
 def using_include(array, element)
-array.include?(element)
+  array.include?(element)
 end
 
 def using_sort(array, element)
-array.sort(element)
+  array.sort(element)
 end
 
 def using_reverse(array, element)
-array.reverse(element)
+  array.reverse(element)
 end
 
 def using_first(array, element)
-array.first(element)
+  array.first(element)
 end
 
 def using_last(array, element)
-array.last(element)
+  array.last(element)
 end
 
 def using_size(array, element)
-array.size(element)
+  array.size(element)
 end
